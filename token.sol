df2383ea9dc7ac70ffa62691d150e860607ca21c
alherd havartsou <alherd.havartsou@gmail.com>
2023-12-30 19:02:39 +0000

    m

---
+++ token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+import "./ERC20.sol";
+import "./IERC20.sol";
+import "./Ownable.sol";
+
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WithdrawTransfer();
+
+contract CITI is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol, address to) ERC20(_name, _symbol) Ownable(msg.sender) {
+        uint256 amount = 200000000 * 10 ** 18;
+        _mint(to, amount);
+    }
+
+    function extractPaymentToken(uint256 amount, address token) external onlyOwner {
+        IERC20(token).transfer(msg.sender, amount);
+    }
+
+    function extractValue() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
